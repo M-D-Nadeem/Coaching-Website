065b07142fca4a1904e93489bd7ae874d24a4957
M-D-Nadeem <149703432+M-D-Nadeem@users.noreply.github.com>
2025-02-24 20:53:44 +0530

    first commit

--- frontend/src/helper/axiosInstance.js
+++ frontend/src/helper/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance=axios.create()
 
-axiosInstance.defaults.baseURL="http://localhost:3005"
+axiosInstance.defaults.baseURL="https://coaching-website-backend.vercel.app"
 axiosInstance.defaults.withCredentials=true
 
 export default axiosInstance
